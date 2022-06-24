95146e4bc1c7dba53fb517a119fec8b378b77dac
Dhruv Gupta <101388885+Dhruva8878@users.noreply.github.com>
2022-06-24 23:06:41 +0530

    added

--- src/Vowel.java
+++ src/Vowel.java
@@ -6,20 +6,36 @@ public class Vowel {
   String n;
 
 
+
     public static void main(String[] args) {
 
-        String [] vowel={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
-                "x","y","z"};
+        String [] vowel={"a","e","i","o","u","A","E","O","I","U"};
+        String [] consonent={"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"};
 
-        Vowel num= new Vowel();
-        num.n="b";
+        Vowel num1= new Vowel();
+        num1.n="U";
+        Vowel num2=new Vowel();
+        num2.n="x";
+        Vowel num3=new Vowel();
+        num3.n="4";
+
 
         for(int i=0; i<vowel.length; i++){
-            if(vowel[i]==num.n){
-                System.out.println(num.n);
+            if(vowel[i]==num1.n){
+                System.out.println("vowel");
                 break;
             }
-
+        }
+        for(int j=0; j<consonent.length; j++){
+            if(consonent[j]== num2.n){
+                System.out.println("consonent");
+            }
+        }
+        for(String x : consonent){
+            if(x != num3.n){
+                System.out.println("Invalid number");
+                break;
+            }
         }
 
     }
