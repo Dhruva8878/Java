b99d50b9e6090d9f9992932b33071bc06c64803f
Dhruv Gupta <101388885+Dhruva8878@users.noreply.github.com>
2022-06-28 22:19:10 +0530

    added

---
+++ Day-4/Demo1.java
@@ -0,0 +1,23 @@
+public class Demo1 {
+
+     void Demo(){
+         System.out.println("Nothing");
+    }
+     void Demo(String s){
+         System.out.println("Inside String"+" "+s);
+    }
+     void Demo(int i){
+         System.out.println("inside Integer"+" "+i);
+    }
+     void Demo(Float f){
+         System.out.println("inside Float"+" "+f);
+    }
+
+    public static void main(String[] args) {
+        Demo1 d1=new Demo1();
+        d1.Demo();
+        d1.Demo("Dhruv");
+        d1.Demo(10);
+        d1.Demo(10.10f);
+    }
+}
